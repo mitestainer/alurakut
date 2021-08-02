719447d84736b0c1e102034d5601138e278af174
Matheus Mitestainer <matheusmitestainer@outlook.com>
2021-08-01 22:56:47 -0300

    Aula 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AluraCommons'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  *, *::before, *::after {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
+  body {
+    background-color: #d9e6f6;
+    font-family: sans-serif;
+  }
+
+  #__next {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
